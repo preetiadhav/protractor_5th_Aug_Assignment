f857f7081c25762c651cc23c9c0de4e374886217
Preeti Adhav <preeti_adhav@epam.com>
2022-08-05 01:39:51 +0530

    modified

--- conf/conf.js
+++ conf/conf.js
@@ -12,7 +12,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['..//testcases/CafeTownLogin_spec.js'],
+  specs: ['..//testcases/ArthematicOperator_spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
